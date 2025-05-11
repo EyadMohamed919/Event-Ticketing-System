22af5acc924567a632c2efcdd21780dc02ee4c55
Selimeleraky <Selimeleraky66@gmail.com>
2025-05-11 10:58:17 +0300

    Creating Profile Phase 1
    Phase 1 (initializing)

---
+++ Profile.py
@@ -0,0 +1,8 @@
+class Profile:
+    def __init__ (self,Name,Age,Email):
+        name = Name
+        age = Age
+        email = Email
+    
+    def Get_info(self,Name,Age,Email):
+        return (Name , Age , Email)
