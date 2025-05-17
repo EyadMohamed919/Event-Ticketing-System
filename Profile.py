ac67f812c630f620ebfacddc31799fbbfc5b767a
Selimeleraky <Selimeleraky66@gmail.com>
2025-05-17 22:45:49 +0300

    Added Profile button

--- Profile.py
+++ Profile.py
@@ -1,32 +1,40 @@
 from customtkinter import *
-from PIL.ImageOps import Image
+from PIL import Image
+import os
 
 class Profile:
     def __init__(self, Name, Age, Email):
         self.name = Name
         self.age = Age
         self.email = Email
-    
+
     def Get_info(self):
         return (self.name, self.age, self.email)
+
+
+
 
 app = CTk()
 app.geometry("1280x720")
 app.title("Event Ticketing System")
 
-user_profile = Profile(Name=input(), Age=input(), Email=input())
 
-ProfileIcon = CTkFrame(width=100, height=100, corner_radius=50, fg_color="#001e63", master=app, border_width=1, border_color="#385ffc")
+name = input("Enter name: ")
+age = input("Enter age: ")
+email = input("Enter email: ")
 
-
-icon_image = Image.open("blank.png")
-icon_image = icon_image.resize((40, 40))
-icon = CTkImage(light_image=icon_image, dark_image=icon_image, size=(40, 40))
+user_profile = Profile(Name=name, Age=age, Email=email)
 
 
-icon_label = CTkLabel(master=ProfileIcon, image=icon, text="")
-icon_label.place(relx=0.5, rely=0.5, anchor="center")
+desktop_path = os.path.join(os.path.expanduser("~"), "Desktop", "blank.png")
+profile_img = CTkImage(light_image=Image.open(desktop_path), size=(60, 60))
 
-ProfileIcon.place(x=100, y=100)
+
+CTkEntry(master=app, text_color="#4a6eff", placeholder_text="Enter event name", width=300)
+
+
+profile_button = CTkButton(master=app, image=profile_img, width=60, height=60, corner_radius=100, fg_color="white", border_width=2, border_color="white")
+
+profile_button.place(x=60, y=50)
 
 app.mainloop()
